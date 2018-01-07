d2c0f29d3074ff6065f7ee2c5f8369c27a6a4461
Gorshkov Anton <goran1301@mail.ru>
2018-01-07 22:38:52 +0300

    made menu buttons

--- core/src/deaddream/screens/MainMenuScreen.java
+++ core/src/deaddream/screens/MainMenuScreen.java
@@ -7,8 +7,12 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 //import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.mygdx.dd.DeadDream;
@@ -20,6 +24,7 @@ public class MainMenuScreen implements Screen{
 	private Stage stage;
 	private ShapeRenderer shapeRenderer;
 	private Skin skin;
+	private TextButton buttonPlay, buttonExit;
 	
 	public MainMenuScreen(final DeadDream game) {
 		this.game = game;
@@ -28,9 +33,41 @@ public class MainMenuScreen implements Screen{
 		this.skin = new Skin();
 	}
 	
+	private void initButtons() {
+		this.buttonPlay = this.generateButton("Play", 10, 50, 150, 20, Color.BLACK);
+		this.buttonExit = this.generateButton("Exit", 10, 20, 150, 20, Color.BLACK);
+		this.buttonExit.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				Gdx.app.exit();
+			}
+		});
+		
+		this.buttonPlay.addListener(new ClickListener(){
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				System.out.println("PLay click");
+			}
+		});
+		this.stage.addActor(this.buttonPlay);
+		this.stage.addActor(this.buttonExit);
+	}
+	
+	private TextButton generateButton(String caption, int x, int y, int width, int height, Color color) {
+		TextButtonStyle buttonStyle = new TextButtonStyle();
+		buttonStyle.font = this.game.font;
+		buttonStyle.fontColor = color;
+		TextButton button = new TextButton(caption, buttonStyle);
+		button.setPosition(x, y);
+		button.setSize(width, height);
+		return button;
+	}
+	
 	@Override
 	public void show() {
+		Gdx.input.setInputProcessor(this.stage);
 		System.out.println("Main menu");
+		this.initButtons();
 		// TODO Auto-generated method stub
 		
 	}
@@ -40,6 +77,13 @@ public class MainMenuScreen implements Screen{
 		// TODO Auto-generated method stub
 		Gdx.gl.glClearColor(255f, 255f, 255f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		this.update(delta);
+		this.stage.draw();
+		
+	}
+	
+	public void update(float delta) {
+		this.stage.act(delta);
 	}
 
 	@Override
@@ -69,7 +113,9 @@ public class MainMenuScreen implements Screen{
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+		this.stage.dispose();
+		this.skin.dispose();
+		this.shapeRenderer.dispose();
 	}
 	
 }
