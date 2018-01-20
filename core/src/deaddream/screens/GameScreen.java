3126141c8bc4d02202d84217c3586d5d9d5cc8ce
Gorshkov Anton <goran1301@mail.ru>
2018-01-21 00:30:44 +0300

    added unit class and mouse controll

--- core/src/deaddream/screens/GameScreen.java
+++ core/src/deaddream/screens/GameScreen.java
@@ -3,20 +3,17 @@ package deaddream.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.dd.Constants;
 import com.mygdx.dd.DeadDream;
+
+import deaddream.units.Unit;
 
 
 public class GameScreen implements Screen {
@@ -25,7 +22,7 @@ public class GameScreen implements Screen {
 	
 	private World world;
 	
-	private Body testUnit, platform;
+	private Unit unit00, unit01;
 	
 	private Box2DDebugRenderer b2ddr;
 	
@@ -33,7 +30,6 @@ public class GameScreen implements Screen {
 	
 	private Texture background;
 	
-	private Texture testUnitSkin;
 	
 	
 	public GameScreen(final DeadDream game) {
@@ -42,97 +38,77 @@ public class GameScreen implements Screen {
 		this.b2ddr = new Box2DDebugRenderer();
 		this.debugMatrix = this.game.camera.combined.cpy();//new Matrix4(this.game.camera.combined.cpy());
 		this.debugMatrix.scale(Constants.PPM, Constants.PPM, 0.0f);
-		
-		
-		this.testUnit = this.createUnit(0, 0, 40, 60, false);
-		this.platform = this.createUnit(200, 200, 40, 60, true);
-	}
-	
-	private Body createUnit(int x, int y, int width, int height, boolean isStatic) {
-		BodyDef def = new BodyDef();
-		if (isStatic) {
-			def.type = BodyType.StaticBody;
-		} else {
-			def.type = BodyType.DynamicBody;
-		}
-		
-		def.position.set(x / Constants.PPM, y / Constants.PPM);
-		//TODO we need rotating unit
-		def.fixedRotation = true;
-		
-		PolygonShape shape = new PolygonShape();
-		shape.setAsBox(width / 2 / Constants.PPM, height / 2 / Constants.PPM);
-		
-		Body unit = this.world.createBody(def);
-		unit.createFixture(shape, 1.0f);
-		shape.dispose();
-		
-		
-		return unit;
-	}
-	
+	}	
 
 	@Override
 	public void show() {
 		System.out.println("Game");
 		this.loadTextures();
-		// TODO Auto-generated method stub
-		
+		this.unit00 = new Unit(this.world, game.assets.get("skins/units/protector.png", Texture.class), 0.0f, 0.0f, 0.0f);
+		this.unit01 = new Unit(this.world, game.assets.get("skins/units/protector.png", Texture.class), 10f, 10f, 1f);
+	
 	}
 	
 	private void loadTextures() {
 		this.background = game.assets.get("backgrounds/bg1.jpg", Texture.class);
-		this.testUnitSkin = game.assets.get("skins/units/protector.png", Texture.class);
+		//this.testUnitSkin = game.assets.get("skins/units/protector.png", Texture.class);
 		//this.background = new Image(background);
 	}
 
 	@Override
 	public void render(float delta) {
 		update(delta);
-		
-		Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1f);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		this.game.batch.begin();
-		this.game.batch.enableBlending();
-		this.game.batch.draw(this.background, 0, 0,
-                this.game.V_WIDTH, this.game.V_HEIGHT);
-		this.game.batch.draw(this.testUnitSkin, this.testUnit.getPosition().x * Constants.PPM - (this.testUnitSkin.getWidth() /2) , this.testUnit.getPosition().y * Constants.PPM - (this.testUnitSkin.getHeight() /2));
-		this.game.batch.draw(this.testUnitSkin, this.platform.getPosition().x * Constants.PPM - (this.testUnitSkin.getWidth() /2) , this.platform.getPosition().y * Constants.PPM - (this.testUnitSkin.getHeight() /2));
-		game.font.setColor(Color.WHITE);
-		game.font.draw(game.batch, "Skin X" + String.valueOf(this.testUnit.getPosition().x * Constants.PPM - (this.testUnitSkin.getWidth() /2)) + " Skin Y" + String.valueOf(testUnit.getPosition().y * Constants.PPM - (testUnitSkin.getHeight() /2) + " Body X" + String.valueOf(testUnit.getPosition().x)) + " Body Y" + String.valueOf(testUnit.getPosition().y), 0, 0);
+		beginBatch();
+		renderBackground();
+		renderUnits();
+		//this.game.batch.draw(this.testUnitSkin, this.testUnit.getPosition().x * Constants.PPM - (this.testUnitSkin.getWidth() /2) , this.testUnit.getPosition().y * Constants.PPM - (this.testUnitSkin.getHeight() /2));
+		//this.game.batch.draw(this.testUnitSkin, this.platform.getPosition().x * Constants.PPM - (this.testUnitSkin.getWidth() /2) , this.platform.getPosition().y * Constants.PPM - (this.testUnitSkin.getHeight() /2));
+		//game.font.setColor(Color.WHITE);
+		//game.font.draw(game.batch, "Skin X" + String.valueOf(this.testUnit.getPosition().x * Constants.PPM - (this.testUnitSkin.getWidth() /2)) + " Skin Y" + String.valueOf(testUnit.getPosition().y * Constants.PPM - (testUnitSkin.getHeight() /2) + " Body X" + String.valueOf(testUnit.getPosition().x)) + " Body Y" + String.valueOf(testUnit.getPosition().y), 0, 0);
 		
 		this.game.batch.end();
 		
 		
+		
 		// TODO Auto-generated method stub
 		
-		this.b2ddr.render(this.world, this.debugMatrix);
+		//this.b2ddr.render(this.world, this.debugMatrix);
+	}
+	
+	private void renderUnits() {
+		this.unit00.render(this.game.batch);
+		this.unit01.render(this.game.batch);
+	}
+	
+	private void beginBatch() {
+		this.game.batch.begin();
+		this.game.batch.enableBlending();
+		
+	}
+	
+	private void renderBackground() {
+		Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		this.game.batch.draw(this.background, 0, 0,
+                this.game.V_WIDTH, this.game.V_HEIGHT);
 	}
 	
 	public void update(float delta) {
 		world.step(1/60f, 6, 2);
+		unit00.update(delta);
+		unit01.update(delta);
 		this.game.batch.setProjectionMatrix(this.game.camera.combined);
-		this.setCameraToMeters(testUnit.getPosition().x, testUnit.getPosition().y);
+		this.setCameraToMeters(unit00.getBody().getPosition().x, unit00.getBody().getPosition().y);
 		this.updateInput();
 	}
 	
 	private void updateInput() {
-		int horizontalForce = 0;
-		int verticalForce = 0;
-		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			horizontalForce -= 1;
-		} 
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			horizontalForce += 1;
+		if (Gdx.input.isButtonPressed(Input.Buttons.RIGHT))
+		{
+		    Vector3 tmp = this.game.camera.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
+		    this.unit00.moveTo(tmp.x / Constants.PPM, tmp.y / Constants.PPM);
 		}
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-			verticalForce += 1;
-		} 
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-			verticalForce -= 1;
-		}
-        this.testUnit.setLinearVelocity(horizontalForce * 5, verticalForce * 5);
+		this.unit00.move();
 	}
 	
 	public void setCameraToMeters(float x, float y) {
@@ -174,6 +150,9 @@ public class GameScreen implements Screen {
 
 	@Override
 	public void dispose() {
+		this.background.dispose();
+		this.unit00.dispose();
+		this.unit01.dispose();
 		world.dispose();
 		b2ddr.dispose();
 		// TODO Auto-generated method stub
---
+++ core/src/deaddream/units/Unit.java
@@ -0,0 +1,161 @@
+package deaddream.units;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.World;
+import com.mygdx.dd.Constants;
+
+/**
+ * 
+ * 
+ * @author goran
+ *
+ */
+public class Unit {
+	protected Sprite staticTexture;
+	protected Body body;
+	protected final int width = 40;
+	protected final int height = 60;
+	protected final float velocity = 5f;
+	protected Vector2 currentMoveGoal;
+	protected final float goalPointFaultRange = 0.5f;
+	protected boolean isMoving;
+	
+	
+	public Unit(World world, Texture staticTexture, Vector2 position, float angle) {
+		constructor(world, staticTexture, position.x, position.y, angle);
+	}
+	
+	public Unit(World world, Texture staticTexture, float x, float y, float angle) {
+		constructor(world, staticTexture, x, y, angle);
+	}
+	
+	private void constructor(World world, Texture staticTexture, float x, float y, float angle) {
+		this.currentMoveGoal = new Vector2();
+		this.isMoving = false;
+		this.staticTexture = new Sprite(staticTexture);
+		this.body = this.createUnit(world, x, y, angle);
+	}
+	
+	/**
+	 * Box2D body creation
+	 * 
+	 * @param world
+	 * @param x meters
+	 * @param y meters
+	 * @param angle
+	 * @return
+	 */
+	protected Body createUnit(World world, float x, float y, float angle) {
+		
+		BodyDef def = new BodyDef();
+		def.position.set(x, y);
+		def.angle = angle;
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(this.width / 2 / Constants.PPM, this.height / 2 / Constants.PPM);
+		def.type = BodyType.DynamicBody;
+		Body body = world.createBody(def);
+		body.createFixture(shape, 1.0f);
+		shape.dispose();
+		
+		
+		return body;
+	}
+	
+	/**
+	 * render unit on screen
+	 * 
+	 * @param batch
+	 */
+	public void render(SpriteBatch batch) {
+		this.staticTexture.setPosition(this.body.getPosition().x * Constants.PPM - (this.staticTexture.getWidth() /2),
+				this.body.getPosition().y * Constants.PPM - (this.staticTexture.getHeight() /2));
+		this.staticTexture.setRotation(MathUtils.radiansToDegrees * this.body.getAngle());
+		this.staticTexture.draw(batch);
+	}
+	
+
+	public void move() {
+		if (!Gdx.input.isKeyPressed(Input.Keys.ANY_KEY)) {
+			return;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
+        	this.moveTo(5f, 5f);
+        	return;
+        }
+		
+		int horizontalForce = 0;
+		int verticalForce = 0;
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			horizontalForce -= 1;
+		} 
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			horizontalForce += 1;
+		}
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			verticalForce += 1;
+		} 
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			verticalForce -= 1;
+		}
+        
+        this.body.setLinearVelocity(horizontalForce * 5, verticalForce * 5);
+	}
+	
+	/**
+	 * Set new point to move
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	public void moveTo(float x, float y) {
+		this.currentMoveGoal.x = x;
+		this.currentMoveGoal.y = y;
+		float rangeX = x - body.getPosition().x;
+		float rangeY = y - body.getPosition().y;
+		float maxXY= Math.max(Math.abs( rangeX ), Math.abs( rangeY ));
+		body.setLinearVelocity(rangeX / maxXY * velocity, rangeY / maxXY * velocity);
+		
+		//float h = (float) Math.sqrt( rangeX * rangeX +  rangeY * rangeY );
+		this.isMoving = true;
+		//body.setLinearVelocity((x - body.getPosition().x) * velocity, (y - body.getPosition().y)* velocity);
+	}
+	
+	/**
+	 * logic for current delta
+	 * 
+	 * @param delta
+	 */
+	public void update(float delta) {
+		if(this.isMoving && (Math.abs(Math.abs(this.currentMoveGoal.x) - Math.abs(this.body.getPosition().x)) <= this.goalPointFaultRange)
+				&& (Math.abs(Math.abs(this.currentMoveGoal.y) - Math.abs(this.body.getPosition().y)) <= this.goalPointFaultRange )) {
+			System.out.println("Stopped body position: " + String.valueOf(body.getPosition().x) + " : " + String.valueOf(body.getPosition().y));
+			System.out.println("Stopped goal position: " + String.valueOf(this.currentMoveGoal.x) + " : " + String.valueOf(this.currentMoveGoal.y));
+			System.out.println("Stopped range: " + String.valueOf(Math.abs(this.currentMoveGoal.x) - Math.abs(this.body.getPosition().x)) + " : " + String.valueOf(Math.abs(this.currentMoveGoal.y) - Math.abs(this.body.getPosition().y)));
+			this.stopMove();
+		}
+	}
+	
+	public void stopMove() {
+		body.setLinearVelocity(0f, 0f);
+		this.isMoving = false;
+	}
+	
+	public Body getBody() {
+		return this.body;
+	}
+	
+	public void dispose() {
+		
+	}
+	
+}
