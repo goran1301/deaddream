a70d7cdb85b2c1079a10c520fd734290b123e094
Gorshkov Anton <goran1301@mail.ru>
2018-02-07 20:46:04 +0300

    clean master

--- core/src/deaddream/units/Protector.java
+++ core/src/deaddream/units/Protector.java
@@ -1,17 +1,15 @@
 package deaddream.units;
 
-import com.badlogic.gdx.Gdx;
+//import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.mygdx.dd.Constants;
-import com.badlogic.gdx.physics.box2d.MassData;
-import aurelienribon.bodyeditor.BodyEditorLoader;
+
+//import aurelienribon.bodyeditor.BodyEditorLoader;
 
 /**
  * The class of Protector unit
@@ -50,48 +48,14 @@ public final class Protector extends Unit {
 	 */
 	@Override
 	protected FixtureDef fixtureDefFactory() {
-		BodyEditorLoader loader = new BodyEditorLoader(Gdx.files.internal("skins/units/protectorBody"));
+		//BodyEditorLoader loader = new BodyEditorLoader(Gdx.files.internal("skins/units/protectorBody"));
 		FixtureDef def = new FixtureDef();
-		
-		//PolygonShape shape = new PolygonShape();
-		//shape.setAsBox(Protector.width / 2 / Constants.PPM, Protector.height / 2 / Constants.PPM);
-		//def.shape = shape;
+		PolygonShape shape = new PolygonShape();
+		shape.setAsBox(Protector.width / 2 / Constants.PPM, Protector.height / 2 / Constants.PPM);
+		def.shape = shape;
 		def.friction = 10f;
 		def.density = 10f;
-		//MassData massData = body.getMassData();
-		//massData.center.set(0, 0);
-		
-		//body.setMassData(massData);
-		loader.attachFixture(body, "protector", def, width / Constants.PPM);
-		return null;
+		return def;
 	}
 	
-	/*@Override
-	public void render(SpriteBatch batch) {
-		staticTexture.setPosition(
-			body.getPosition().x * Constants.PPM - (staticTexture.getWidth() / 2),
-			body.getPosition().y
-		);
-		staticTexture.setRotation(MathUtils.radiansToDegrees * this.body.getAngle());
-		staticTexture.draw(batch);
-	}*/
-	
-	/**
-	 * render unit on screen
-	 * 
-	 * @param batch
-	 */
-	@Override
-	public void render(SpriteBatch batch) {
-		
-		staticTexture.setPosition(
-				body.getPosition().x * Constants.PPM,
-				body.getPosition().y * Constants.PPM
-			);/*this.staticTexture.setPosition(this.body.getPosition().x * Constants.PPM,
-				this.body.getPosition().y * Constants.PPM);*/
-		/*this.staticTexture.setPosition(this.body.getPosition().x * Constants.PPM,
-				this.body.getPosition().y * Constants.PPM);*/
-		this.staticTexture.setRotation(MathUtils.radiansToDegrees * this.body.getAngle());
-		this.staticTexture.draw(batch);
-	}
 }
